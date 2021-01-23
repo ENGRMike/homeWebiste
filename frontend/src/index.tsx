a2f666c24cfac6b29d1e3028e1dfff890ebf3625
Michael <michael.s.bruins@gmail.com>
2021-01-23 14:07:04 -0500

    Added Theme and started store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseLine from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import App from './App';
-import theme from './theme';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from './store/store'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseLine from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import App from "./App";
 
-const store = configureStore();
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "./redux";
+
+import reportWebVitals from "./reportWebVitals";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,7 +24,7 @@ ReactDOM.render(
       </Router>
     </ThemeProvider>
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
 
 // If you want to start measuring performance in your app, pass a function
