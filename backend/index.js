c48b6f4489bde353ceda7770527a2f51922ff9d5
Michael <doofey@localhost.localdomain>
2021-01-23 12:44:31 -0500

    initial graphql setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
-var {graphqlHTTP} = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
 
-const app = express();
+var root = {
+    hello: () => {
+        return 'Hello World!';
+    }
+};
 
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
